0935e604484b9d2c1e5c70732da80319d64617d6
pey <pey@xdepape.com>
2023-02-18 08:25:23 +0800

    initial commit

---
+++ src/main/java/com/pey/rengoku/RengokuApplication.java
@@ -0,0 +1,13 @@
+package com.pey.rengoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RengokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RengokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pey/rengoku/RengokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pey.rengoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RengokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
