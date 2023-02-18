1df7ae39bf4309e674e1ec9e62afbdf528497a16
pey <pey@xdepape.com>
2023-02-18 12:28:07 +0800

    import springdoc-openapi. Easy to test with http://localhost:8080/v3/api-docs

---
+++ src/main/java/com/pey/rengoku/DefaultApiController.java
@@ -0,0 +1,20 @@
+package com.pey.rengoku;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/rengoku")
+public class DefaultApiController {
+
+    @GetMapping
+    public Map<String, String> getHello() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "hellooo");
+        return response;
+    }
+}
